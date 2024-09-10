f3b7dcfb9966926da63c1549ac8dc2c5cc080549
Bashlyklov Mykyta <bashlykovnikita@gmail.com>
2024-09-10 13:01:15 +0300

    Cap Weights

--- data.py
+++ data.py
@@ -1,4 +1,5 @@
 import pandas as pd
+import yfinance as yf
 
 index_names = {
     "SP500": "^GSPC",
@@ -27,3 +28,19 @@ icr_m = {
 
 def get_returns_df(url):
     return pd.read_csv(url, index_col=0)
+
+
+def get_mkt_cap(url):
+    mkt_cap_df = []
+    comp_names = get_returns_df(url).columns
+    for company in comp_names:
+        buff = yf.Ticker(company).info["marketCap"]
+        mkt_cap_df.append(buff)
+
+    return pd.DataFrame(mkt_cap_df, index=comp_names, columns=["Market Cap"])
+
+
+def get_cap_weights(market_cap):
+    total_mkt_cap = market_cap.sum(axis=0)
+    cap_weight = market_cap.divide(total_mkt_cap)
+    return cap_weight
